86b07dfcff44e49573e093e5e38d285d43138289
Ujj9913 <ujjvalvaghsiya@gmail.com>
2021-11-27 19:27:33 +0530

    day 2commited

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:sangath/home_activity.dart';
 
 void main() {
   runApp(MyApp());
@@ -9,14 +10,10 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    int days = 30;
+    String name = "ujjval vaghsiya";
     return MaterialApp(
-      home: Material(
-        child: Center(
-          child: Container(
-            child: Text("hello i am ujjval vaghsiya"),
-          ),
-        ),
-      ),
+      home: home_page(),
     );
   }
 }
