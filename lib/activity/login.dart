431ef4f57da0684828e32ad79a7f058f0ffe4e59
Ujj9913 <ujjvalvaghsiya@gmail.com>
2021-11-30 15:53:57 +0530

    day5 and button

--- lib/activity/login.dart
+++ lib/activity/login.dart
@@ -1,6 +1,7 @@
 import 'dart:ui';
 
 import 'package:flutter/material.dart';
+import 'package:sangath/utils/routes.dart';
 
 class Login extends StatelessWidget {
   const Login({Key? key}) : super(key: key);
@@ -9,50 +10,53 @@ class Login extends StatelessWidget {
   Widget build(BuildContext context) {
     return Material(
       color: Colors.white,
-      child: Column(
-        children: [
-          const Image(image: AssetImage('assets/images/login.png')),
-          const SizedBox(
-            height: 20.0,
-          ),
-          const Text(
-            "Welcome",
-            style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
-          ),
-          const SizedBox(
-            height: 20.0,
-          ),
-          Padding(
-            padding:
-                const EdgeInsets.symmetric(vertical: 16.0, horizontal: 32.0),
-            child: Column(
-              children: [
-                TextFormField(
-                  decoration: const InputDecoration(
-                    hintText: "enter name",
-                    labelText: "Username",
-                  ),
-                ),
-                TextFormField(
-                  obscureText: true,
-                  decoration: const InputDecoration(
-                    hintText: "enter password",
-                    labelText: "Password",
-                  ),
-                ),
-                const SizedBox(
-                  height: 20.0,
-                ),
-                ElevatedButton(
-                    onPressed: () {
-                      print('hello');
-                    },
-                    style: TextButton.styleFrom(),
-                    child: const Text("Login"))
-              ],
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            const Image(image: AssetImage('assets/images/login.png')),
+            const SizedBox(
+              height: 20.0,
             ),
-          )
-        ],
+            const Text(
+              "Welcome",
+              style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(
+              height: 20.0,
+            ),
+            Padding(
+              padding:
+                  const EdgeInsets.symmetric(vertical: 16.0, horizontal: 32.0),
+              child: Column(
+                children: [
+                  TextFormField(
+                    decoration: const InputDecoration(
+                      hintText: "enter name",
+                      labelText: "Username",
+                    ),
+                  ),
+                  TextFormField(
+                    obscureText: true,
+                    decoration: const InputDecoration(
+                      hintText: "enter password",
+                      labelText: "Password",
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 20.0,
+                  ),
+                  ElevatedButton(
+                      onPressed: () {
+                        Navigator.pushNamed(context, MyRouts.homeRoute);
+                      },
+                      style: TextButton.styleFrom(
+                          minimumSize: const Size(150, 40)),
+                      child: const Text("Login"))
+                ],
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
